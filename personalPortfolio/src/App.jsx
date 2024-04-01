9c4599c841c92732a9f6dd48b10e2b796c3dbd43
Evelyn Jiang <evelynjiang4@gmail.com>
2024-04-01 17:55:47 -0400

    Added links to images using map

--- personalPortfolio/src/App.jsx
+++ personalPortfolio/src/App.jsx
@@ -3,54 +3,62 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function NavBar() {
-
-}
-
 function Name() {
   return (
     <div>
-        <h1>Evelyn Jiang</h1>
-      </div>
-  )
+      <h1>Evelyn Jiang</h1>
+    </div>
+  );
 }
 
-function Image({ src, alt, description }) {
+function Image({ src, alt, description, link }) {
   return (
     <div className="image-container">
-      <img src={src} alt={alt} />
-      <input
-        type="text"
-        className="description-input"
-        value={description}
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={src} alt={alt} />
+        <input
+          type="text"
+          className="description-input"
+          value={description}
+          readOnly
         />
+      </a>
     </div>
   );
 }
 
 function Homepage() {
+  const images = [
+    {
+      src: 'https://i.pinimg.com/564x/ee/ab/3b/eeab3b1bd24435f6291855e27e741611.jpg',
+      alt: 'About me',
+      description: 'About me',
+      link: 'https://aces.umd.edu/evelyn-jiang',
+    },
+    {
+      src: 'https://i.pinimg.com/564x/10/77/08/1077080adb48503f7d659c6116a32635.jpg',
+      alt: 'Projects',
+      description: 'Projects',
+      link: 'https://github.com/eve1ynjiang?tab=repositories',
+    },
+  ];
+
   return (
     <div>
       <Name />
-      <Image
-        src="https://i.pinimg.com/564x/ee/ab/3b/eeab3b1bd24435f6291855e27e741611.jpg"
-        alt="About me"
-        description="About me"
-      />
-
-      <Image
-        src="https://i.pinimg.com/564x/10/77/08/1077080adb48503f7d659c6116a32635.jpg"
-        alt="Projects"
-        description="Projects"
-      />
-  </div>  
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.src}
+          alt={image.alt}
+          description={image.description}
+          link={image.link}
+        />
+      ))}
+    </div>
   );
 }
 
-
 export default function MainPage() {
-  return (
-    <Homepage />
-  )
+  return <Homepage />;
 }
-
